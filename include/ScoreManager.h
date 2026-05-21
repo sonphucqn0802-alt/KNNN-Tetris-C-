37483332efd544a99630bafdb3ec14ec9e795475
25520640-NKHung <25520640@gm.uit.edu.vn>
2026-05-21 13:06:56 +0700

    Cap Nhat Class ScoreManager

--- include/ScoreManager.h
+++ include/ScoreManager.h
@@ -1,3 +1,22 @@
-/*
- * Khai bao cach tinh diem, cap do va tong so dong da xoa.
- */
+#pragma once
+
+class ScoreManager {
+public:
+    ScoreManager();
+
+    void reset();
+    void addClearedLines(int count);
+    void addSoftDrop();
+    void addHardDrop(int rows);
+
+    int getScore() const;
+    int getLinesCleared() const;
+    int getLevel() const;
+    int getDisplayLevel() const;
+    int getFallDelayMs() const;
+
+private:
+    int score_;
+    int linesCleared_;
+    int level_;
+};
--- src/ScoreManager.cpp
+++ src/ScoreManager.cpp
@@ -1,3 +1,53 @@
-/*
- * Cai dat quy tac tinh diem, tang level va theo doi so dong da xoa.
- */
+#include "ScoreManager.h"
+
+#include <algorithm>
+
+ScoreManager::ScoreManager()
+    : score_(0),
+      linesCleared_(0),
+      level_(0) {}
+
+void ScoreManager::reset() {
+    score_ = 0;
+    linesCleared_ = 0;
+    level_ = 0;
+}
+
+void ScoreManager::addClearedLines(int count) {
+    static constexpr int kLineScores[] = {0, 40, 100, 300, 1200};
+    if (count < 1 || count > 4) {
+        return;
+    }
+
+    score_ += kLineScores[count] * (level_ + 1);
+    linesCleared_ += count;
+    level_ = linesCleared_ / 10;
+}
+
+void ScoreManager::addSoftDrop() {
+    score_ += 1;
+}
+
+void ScoreManager::addHardDrop(int rows) {
+    score_ += rows * 2;
+}
+
+int ScoreManager::getScore() const {
+    return score_;
+}
+
+int ScoreManager::getLinesCleared() const {
+    return linesCleared_;
+}
+
+int ScoreManager::getLevel() const {
+    return level_;
+}
+
+int ScoreManager::getDisplayLevel() const {
+    return level_ + 1;
+}
+
+int ScoreManager::getFallDelayMs() const {
+    return std::max(80, 800 - level_ * 60);
+}
