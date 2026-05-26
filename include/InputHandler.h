414bfea816227f71ad87d5be463fe2b641010b2d
ChuongLe279 <lebuuchuong2024@gmail.com>
2026-05-26 08:24:19 +0700

    Add input handler h class

--- include/InputHandler.h
+++ include/InputHandler.h
@@ -1,3 +1,22 @@
 /*
  * Dinh nghia giao dien doc phim va chuyen thanh hanh dong trong game.
  */
+#pragma once
+
+enum class InputAction {
+    None,
+    MoveLeft,
+    MoveRight,
+    SoftDrop,
+    HardDrop,
+    RotateClockwise,
+    RotateCounterClockwise,
+    Pause,
+    Restart,
+    Quit
+};
+
+class InputHandler {
+public:
+    InputAction pollAction() const;
+};
